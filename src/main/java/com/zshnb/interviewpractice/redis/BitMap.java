4e02e8077a5ebf6f5533d2668eafd950531d108b
zheng sihua <a857681664@gmail.com>
2021-09-28 11:13:26 +0800

    feat: implement redis bitmap (#1)
    * create BitMap and complete sign, count method
    
    * count continuous sign

---
+++ src/main/java/com/zshnb/interviewpractice/redis/BitMap.java
@@ -0,0 +1,80 @@
+package com.zshnb.interviewpractice.redis;
+
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Locale;
+
+@Component
+public class BitMap {
+    private final String keyFormat = "sign:%d:%s";
+    private final RedisTemplate<String, Serializable> redisTemplate;
+
+    public BitMap(RedisTemplate<String, Serializable> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void sign(int userId, LocalDate date) {
+        String key = String.format(keyFormat, userId, DateTimeFormatter.ofPattern("yyyyMM", Locale.CHINA).format(date));
+        int dayOfMonth = date.getDayOfMonth();
+        redisTemplate.opsForValue().setBit(key, dayOfMonth, true);
+    }
+
+    /**
+     * 统计指定月份用户签到天数
+     * */
+    public int count(int userId, LocalDate date) {
+        String key = String.format(keyFormat, userId, DateTimeFormatter.ofPattern("yyyyMM", Locale.CHINA).format(date));
+        return redisTemplate.execute((RedisCallback<Integer>) connection -> Math.toIntExact(connection.bitCount(key.getBytes(StandardCharsets.UTF_8))));
+    }
+
+    /**
+     * 统计当月指定用户最长连续签到天数
+     * */
+    public int countContinuousInMonth(int userId, int month) {
+        String date = DateTimeFormatter.ofPattern("yyyyMM", Locale.CHINA).format(LocalDate.of(LocalDate.now().getYear(), month, 1));
+        String key = String.format(keyFormat, userId, date);
+        byte[] bytes = ((String) redisTemplate.opsForValue().get(key)).getBytes(StandardCharsets.UTF_8);
+        StringBuilder bits = new StringBuilder();
+        for (byte aByte : bytes) {
+            bits.append(byteToBit(aByte));
+        }
+        return countLongestContinuousSequence(bits.toString());
+    }
+
+    private int countLongestContinuousSequence(String bits) {
+        int max = 0;
+        int count = 0;
+        int index = 0;
+        while (index < bits.length()) {
+            if (bits.charAt(index) == '1') {
+                count++;
+            } else {
+                if (count > max) {
+                    max = count;
+                }
+                count = 0;
+            }
+            index++;
+        }
+        return max;
+    }
+
+    private String byteToBit(byte b) {
+        return "" + (byte) ((b >> 7) & 0x1) +
+            (byte) ((b >> 6) & 0x1) +
+            (byte) ((b >> 5) & 0x1) +
+            (byte) ((b >> 4) & 0x1) +
+            (byte) ((b >> 3) & 0x1) +
+            (byte) ((b >> 2) & 0x1) +
+            (byte) ((b >> 1) & 0x1) +
+            (byte) ((b) & 0x1);
+    }
+}
---
+++ src/test/java/com/zshnb/interviewpractice/redis/BitMapTest.java
@@ -0,0 +1,66 @@
+package com.zshnb.interviewpractice.redis;
+
+import com.zshnb.interviewpractice.BaseTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BitMapTest extends BaseTest {
+    @Autowired
+    private BitMap bitMap;
+    @Autowired
+    private RedisTemplate<String, Serializable> redisTemplate;
+    int userId = 1;
+    int secondUserId = 2;
+
+    @BeforeEach
+    public void beforeSetUp() {
+        redisTemplate.execute((RedisCallback<Object>) connection -> {
+            connection.flushDb();
+            return null;
+        });
+    }
+
+    @Test
+    public void signAndCount() {
+        IntStream.range(20, 26).forEach(it -> {
+            bitMap.sign(userId, LocalDate.of(2021, 9, it));
+        });
+        IntStream.range(20, 26).forEach(it -> {
+            bitMap.sign(secondUserId, LocalDate.of(2021, 8, it));
+        });
+        int userIdSignCount = bitMap.count(userId, LocalDate.now());
+        assertThat(userIdSignCount).isEqualTo(6);
+
+        int secondUserIdSignCount = bitMap.count(secondUserId, LocalDate.now());
+        assertThat(secondUserIdSignCount).isZero();
+
+        secondUserIdSignCount = bitMap.count(secondUserId, LocalDate.of(2021, 8, 1));
+        assertThat(secondUserIdSignCount).isEqualTo(6);
+    }
+
+    @Test
+    public void longestContinuousSignInMonthUsers() {
+        mockSign();
+        int result = bitMap.countContinuousInMonth(userId, 9);
+        assertThat(result).isEqualTo(5);
+    }
+
+    private void mockSign() {
+        IntStream.range(1, 4).forEach(it -> {
+            bitMap.sign(userId, LocalDate.of(2021, 9, it));
+        });
+
+        IntStream.range(10, 15).forEach(it -> {
+            bitMap.sign(userId, LocalDate.of(2021, 9, it));
+        });
+    }
+}
