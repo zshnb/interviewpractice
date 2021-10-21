71486c583f53bdb2a79e74c8e72b17e657a79378
zshnb <a857681664@gmail.com>
2021-10-21 13:59:37 +0800

    create OrderInfo

---
+++ src/main/java/com/zshnb/interviewpractice/miaosha/OrderInfo.java
@@ -0,0 +1,40 @@
+package com.zshnb.interviewpractice.miaosha;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "order_info")
+public class OrderInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(name = "create_at", columnDefinition = "datetime")
+    private LocalDateTime createAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+}
